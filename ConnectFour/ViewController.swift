cc6f74b64138aaef29873bffd54df8580c05abfa
Jackson Ehrenworth <jehrenworth18@avenues.org>
2016-08-25 16:20:00 -0400

    Initial Commit

---
+++ ConnectFour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectFour
+//
+//  Created by Jackson Ehrenworth 18 on 8/25/16.
+//  Copyright © 2016 Jackson Ehrenworth 18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
